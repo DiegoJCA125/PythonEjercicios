c74d6bbed9dc55420ca756f9adff6bdbc04c0408
Diego <diegocarmona125@gmail.com>
2026-04-30 17:02:33 -0500

    continuacion de graficos

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -5,14 +6,14 @@ import matplotlib.pyplot as plt
 # st - Es el objetivo principal de streamlit y se controye toda la interfaz visual
 
 # --- CONFIGURACION DE LA PAGINA
-st.set_p�ge_config(
+st.set_page_config(
     page_title = "Dashboard de Ventas",     #titulo en las pestana del navegador
-    page_icon = "??",                       # icono en la pestana
+    page_icon = "📊",                       # icono en la pestana
     layout = "wide"                         # layout = "wide" usa todo el ancho de la pantalla
 )
 
 # ---- TITULO PRINCIPAL ----------
-st.title("?? Dashboard de Ventas")      #titulo grande en la pagina
+st.title("📊 Dashboard de Ventas")      #titulo grande en la pagina
 st.markdown("-------")                  # es una linea divisora
 
 # ------- DATOS ---------
@@ -41,3 +42,72 @@ ventas = pd.DataFrame({
 })
 
 # COMBINAR TABLAS
+ventas_completas = pd.merge(ventas, productos, on = "producto_id")
+ventas_completas = pd.merge(ventas_completas, clientes, on = "cliente_id")
+ventas_completas = ventas_completas.rename(columns={
+    "nombre_x": "producto",
+    "nombre_y": "cliente"
+    })
+ventas_completas["total"] = ventas_completas["precio"] * ventas_completas["cantidad"]
+
+# ---- METRICAS PRINCIPALES
+# st.columns() - divide la pantalla en columnas
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    #st.metric() - muestra una metrica con ttulo y valor
+    st.metric("💰 Total Ventas", f"${ventas_completas['total'].sum():,}")
+
+with col2:
+    mejor_cliente = ventas_completas.groupby("cliente")["total"].sum().idxmax()
+    #idxmax() - devuelve el valor maximo 
+    st.metric("💰 Mejor Cliente", mejor_cliente)
+
+with col3:
+    mejor_producto = ventas_completas.groupby("producto")["total"].sum().idxmax()
+    st.metric("⭐ Producto Estrella", mejor_producto)
+
+st.markdown("-----")
+
+# --------- GRAFICAS ------------
+# SE DIVIDE LA PANTALLA EN 2 COLUMNAS PARA LAS GRAFICAS
+col1, col2 = st.columns(2)
+
+with col1:
+    st.subheader("📈 Ventas por Mes")
+    #st.subheader() - Titulo mediano en la pagina
+    por_mes = ventas_completas.groupby("fecha")["total"].sum()
+    fig, ax = plt.subplots()
+    #fig, ax = plt.subplots() - crea una figura y in eje para la grafica
+    por_mes.plot(kind = "line", ax = ax, marker= "o", color = "steelblue")
+    ax.set_xlabel("mes")
+    ax.set_ylabel("Total ventas")
+    st.pyplot(fig)
+    #st.pyplot - muestra la grafica de matplotlib en el dashboard
+
+with col2:
+    st.subheader("🏆 Top Productos")
+    por_producto = ventas_completas.groupby("producto")["total"].sum()
+    por_producto = por_producto.sort_values(ascending=False)
+    fig2, ax2 = plt.subplots()
+    por_producto.plot(kind = "bar", ax = ax2, color = "green")
+    ax2.set_xlabel("Producto")
+    ax2.set_ylabel("Total Vendido")
+    ax2.tick_params(axis = "x", rotation = 45)
+    st.pyplot(fig2)
+
+st.markdown("------------")
+
+#--------TABLA INTERACTIVA -------------
+st.subheader("📋 Detalle de Ventas")
+#st.dataframe() - muestra una tabla interactiva que se puede ordenar
+st.dataframe(ventas_completas[["venta_id", "producto", "cliente", "cantidad", "precio", "total", "fecha"]])
+
+# --------FILTRO INTERACTIVO ------------
+st.subheader("🔍 Filtrar por Cliente")
+#st.selectbox() - Crea un menu desplegable interactivo
+cliente_seleccionado = st.selectbox(
+    "SElecciona un cliente:",
+    ventas_completas["cliente".unique]
+    #unique, devuelve los valores unicos de la columna
+)
