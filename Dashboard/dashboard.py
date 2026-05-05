717f479db09039559dad96bc23eb614578ca82c4
Diego <diegocarmona125@gmail.com>
2026-05-05 11:09:11 -0500

    Dashboard con reporte en Excel para descargar

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -116,3 +116,81 @@ cliente_seleccionado = st.selectbox(
 filtrado = ventas_completas[ventas_completas["cliente"] == cliente_seleccionado]
 st.dataframe(filtrado[["producto", "cantidad", "precio", "total", "fecha"]])
 st.metric("Total comprado por cliente", f"${filtrado['total'].sum():,}")
+
+st.markdown("-----")
+
+#FILTRAR POR FECHA ----
+st.subheader("📅 Filtrar por Mes")
+#st.multiselect() - permite seleccionar diferentes opciones
+meses_seleccionados = st.multiselect(
+    "Selecciona los meses:",
+    options = ventas_completas["fecha"].unique(),
+    default = ventas_completas["fecha"].unique()
+    # default - Valoes seleccionados pordefecto (todos)
+)
+
+# FILTRAR POR LO MESES SELECCIONADOS
+#isin() - verifica si el valor estan dentro de la lista
+filtrado_fecha = ventas_completas[ventas_completas["fecha"].isin(meses_seleccionados)]
+
+# MOSTRAR METRICAS DEL PERIODO SELECICONADO
+col1, col2 = st.columns(2)
+with col1:
+    st.metric("💰 Total periodo", f"${filtrado_fecha['total'].sum():,}")
+with col2:
+    st.metric("🛒 Ventas realizadas", len(filtrado_fecha))
+
+st.markdown("******")    
+
+# ------------ RESUMEN EJECUTIVO --------------
+st.subheader("📊 Resumen Ejecutivo")
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    # PRODUCTO MA VENDIDO EN EL PERIODO FILTRADO
+    if len(filtrado_fecha) > 0:
+        top_producto = filtrado_fecha.groupby("producto")["total"].sum().idxmax()
+        st.metric("⭐ Top Producto", top_producto)
+
+with col2:
+    #CIUDAD CON MAS VENTAS
+    if len(filtrado_fecha) > 0:
+        top_ciudad = filtrado_fecha.groupby("ciudad")["total"].sum().idxmax()
+        st.metric("🗺️ Top Ciudad", top_ciudad)
+
+with col3:
+    # PROMEDIO DE VENTA
+    if len(filtrado_fecha) > 0:
+        promedio = filtrado_fecha["total"].mean()
+        st.metric("📈 Venta Promedio", f"${promedio:,.0f}")
+        # --- :,.0f - formato de numero con comas y sin decimales
+
+st.markdown("///////////////")
+
+#--------- DESCARGAR REPORTE -------------------
+st.subheader("💾 Descargar Reporte")
+
+# CONVERNTIR DATAFRAME A EXCEL EN MEMORIA
+# BytesIO() - CREA UN ARCHIVO EN MEMORIA SIN GUARDARLO EN DISCO
+import io
+buffer = io.BytesIO()
+# io - Libreria para manejar archivos en memoria
+
+with pd.ExcelWriter(buffer, engine = "openpyxl") as writer:
+    filtrado_fecha[["venta_id", "producto", "cliente", "cantidad", "precio", "total", "fecha"]].to_excel(writer, sheet_name = "Ventas", index = False)
+    filtrado_fecha.groupby("producto")["total"].sum()\
+        .reset_index().to_excel(writer, sheet_name ="Por Prodcuto", index = False)
+    filtrado_fecha.groupby("ciudad")["total"].sum()\
+        .reset_index().to_excel(writer, sheet_name = "Por Ciudad", index = False)
+
+#st.download_botton() - CREA UN BOTON DE DESCARGA EN EL DASHBOARD
+st.download_button(
+    label = "📥 Descargar Reporte Excel",
+    data = buffer.getvalue(),
+    # getvalue() - OBTIENE EL CONTENIDO DEL ARCHIVO EN MEMORIA
+    file_name = "reporte_ventas.xlsx",
+    mime = "application/vnd.ms-excel"
+    #mime - TIPO DE ARCHIVO QUE SE DESCARGA
+)
+    
